9a51390de97c3371018241e7b197d21a07e7bf48
EasonLin <CoderEasonLin@users.noreply.github.com>
2017-11-22 08:59:33 +0800

    Add second test: [TestCase("abc", "abc", "abc")]

--- BasicNicoVariation/BasicNicoVariationTests.cs
+++ BasicNicoVariation/BasicNicoVariationTests.cs
@@ -6,6 +6,7 @@ namespace BasicNicoVariation
     public class BasicNicoVariationTests
     {
         [TestCase("", "", "")]
+        [TestCase("abc", "abc", "abc")]
         public void BasicNicoVariationTest(string expected, string key, string message)
         {
             Assert.AreEqual(expected, BasicNico.Encrypt(key, message));

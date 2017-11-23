b46e1d977e9667ed1bea300bd31ac003bde26978
EasonLin <CoderEasonLin@users.noreply.github.com>
2017-11-23 23:27:20 +0800

    add test: [TestCase("ba", "1234567890", "2143658709")] [TestCase("a", "message", "message")]
    test passed.

--- BasicNicoVariation/BasicNicoVariationTests.cs
+++ BasicNicoVariation/BasicNicoVariationTests.cs
@@ -12,6 +12,8 @@ namespace BasicNicoVariation
         [TestCase("abc", "abcdef", "abcdef")]
         [TestCase("abc", "abcd", "abcd  ")]
         [TestCase("crazy", "secretinformation", "cseerntiofarmit on  ")]
+        [TestCase("a", "message", "message")]
+        [TestCase("ba", "1234567890", "2143658709")]
         public void BasicNicoVariationTest(string key, string message, string expected)
         {
             Assert.AreEqual(expected, BasicNico.Encrypt(key, message));

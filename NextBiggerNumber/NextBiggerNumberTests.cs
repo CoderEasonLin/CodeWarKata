99a77dcac0935e55732126aad6318d9f588a5f5e
EasonLin <CoderEasonLin@users.noreply.github.com>
2017-11-23 23:47:10 +0800

    add test: [TestCase(513, 531)] [TestCase(2017, 2071)] and [TestCase(2017, 2071)] failed.

--- NextBiggerNumber/NextBiggerNumberTests.cs
+++ NextBiggerNumber/NextBiggerNumberTests.cs
@@ -7,6 +7,8 @@ namespace NextBiggerNumber
     {
         [TestCase(0, -1)]
         [TestCase(12, 21)]
+        [TestCase(513, 531)]
+        [TestCase(2017, 2071)]
         public void NextBiggerNumberTest(int input, int expected)
         {
             Assert.AreEqual(expected, NextBiggerNumber.By(input));

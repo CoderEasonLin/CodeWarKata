7e6e873dc524e028026af682f67fae77e582f82e
EasonLin <CoderEasonLin@users.noreply.github.com>
2017-11-25 12:27:16 +0800

    passed test. [TestCase(2017, 2071)]

--- NextBiggerNumber/NextBiggerNumber.cs
+++ NextBiggerNumber/NextBiggerNumber.cs
@@ -5,10 +5,26 @@ namespace NextBiggerNumber
 {
     public class NextBiggerNumber
     {
-        public static int By(int input)
+        public static int By(int number)
         {
-            var result = Convert.ToInt32(string.Concat(input.ToString().OrderByDescending(x => x)));
-            return input == result ? -1 : result;
+            var digits = number.ToString().Select(x => Convert.ToInt32(x.ToString())).ToArray();
+            for (int i = digits.Length - 1; i > 0; i--)
+            {
+                if (digits[i - 1] < digits[i])
+                {
+                    Swap(digits, i - 1, i);
+                    break;
+                }
+            }
+            var result = Convert.ToInt32(string.Concat(digits));
+            return number == result ? -1 : result;
+        }
+
+        private static void Swap(int[] digits, int index1, int index2)
+        {
+            int temp = digits[index1];
+            digits[index1] = digits[index2];
+            digits[index2] = temp;
         }
     }
 }

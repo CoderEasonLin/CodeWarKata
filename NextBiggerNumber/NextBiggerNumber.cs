e49e0689b9fa09367fbffa4ab2fd2705f2aff701
EasonLin <CoderEasonLin@users.noreply.github.com>
2017-11-25 13:24:33 +0800

    add tests:         [TestCase(414, 441)]         [TestCase(144, 414)]         [TestCase(111, -1)]         [TestCase(531, -1)]         [TestCase(12321, 13122)]         [TestCase(1232123212321, 1232123213122)]         [TestCase(59884848459853, 59884848483559)] found [TestCase(59884848459853, 59884848483559)] failed.
    and passed the test.

--- NextBiggerNumber/NextBiggerNumber.cs
+++ NextBiggerNumber/NextBiggerNumber.cs
@@ -5,20 +5,36 @@ namespace NextBiggerNumber
 {
     public class NextBiggerNumber
     {
-        public static int By(int number)
+        public static long By(long number)
         {
             var digits = number.ToString().Select(x => Convert.ToInt32(x.ToString())).ToArray();
             for (int i = digits.Length - 1; i > 0; i--)
             {
                 if (digits[i - 1] < digits[i])
                 {
-                    Swap(digits, i - 1, i);
+                    var nextBiggerDigitIndex = NextBiggerDigitIndex(digits, i, digits[i-1]);
+                    Swap(digits, i - 1, nextBiggerDigitIndex == -1 ? i : nextBiggerDigitIndex);
                     Array.Sort(digits, i, digits.Length - i);
                     break;
                 }
             }
-            var result = Convert.ToInt32(string.Concat(digits));
+            var result = Convert.ToInt64(string.Concat(digits));
             return number == result ? -1 : result;
+        }
+
+        private static int NextBiggerDigitIndex(int[] digits, int startFrom, int value)
+        {
+            var result = -1;
+            var biggerDigit = 10;
+            for (int i = startFrom; i < digits.Length; i++)
+            {
+                if (digits[i] > value && digits[i] < biggerDigit)
+                {
+                    result = i;
+                    biggerDigit = digits[i];
+                }
+            }
+            return result;
         }
 
         private static void Swap(int[] digits, int index1, int index2)
--- NextBiggerNumber/NextBiggerNumberTests.cs
+++ NextBiggerNumber/NextBiggerNumberTests.cs
@@ -10,7 +10,14 @@ namespace NextBiggerNumber
         [TestCase(513, 531)]
         [TestCase(2017, 2071)]
         [TestCase(20170, 20701)]
-        public void NextBiggerNumberTest(int input, int expected)
+        [TestCase(414, 441)]
+        [TestCase(144, 414)]
+        [TestCase(111, -1)]
+        [TestCase(531, -1)]
+        [TestCase(12321, 13122)]
+        [TestCase(1232123212321, 1232123213122)]
+        [TestCase(59884848459853, 59884848483559)]
+        public void NextBiggerNumberTest(long input, long expected)
         {
             Assert.AreEqual(expected, NextBiggerNumber.By(input));
         }

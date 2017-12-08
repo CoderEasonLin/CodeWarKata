483cbdfe7c823f9f335c6cb9ef9176c474113b6f
EasonLin <CoderEasonLin@users.noreply.github.com>
2017-12-08 22:14:15 +0800

    Add and pass NumbersWithinColumnAreAllInTheCorrectRange.

--- BingoCard/BingoCard.cs
+++ BingoCard/BingoCard.cs
@@ -8,21 +8,21 @@ namespace BingoCard
     {
         public static string[] GetCard()
         {
-            return GenerateNumbers(startWith: "B", count: 5)
-                .Concat(GenerateNumbers(startWith: "I", count: 5)
-                .Concat(GenerateNumbers(startWith: "N", count: 4)
-                .Concat(GenerateNumbers(startWith: "G", count: 5)
-                .Concat(GenerateNumbers(startWith: "O", count: 5)
+            return GenerateNumbers(startWith: "B", count: 5, min: 1, max: 15)
+                .Concat(GenerateNumbers(startWith: "I", count: 5, min: 16, max: 30)
+                .Concat(GenerateNumbers(startWith: "N", count: 4, min: 31, max: 45)
+                .Concat(GenerateNumbers(startWith: "G", count: 5, min: 46, max: 60)
+                .Concat(GenerateNumbers(startWith: "O", count: 5, min: 61, max: 75)
                 )))).ToArray();
         }
 
-        private static HashSet<string> GenerateNumbers(string startWith, int count)
+        private static HashSet<string> GenerateNumbers(string startWith, int count, int min, int max)
         {
             var random = new Random();
             var hashSet = new HashSet<string>();
             while (hashSet.Count < count)
             {
-                hashSet.Add($"{startWith}{random.Next(1000)}");
+                hashSet.Add($"{startWith}{random.Next(min, max)}");
             }
             return hashSet;
         }

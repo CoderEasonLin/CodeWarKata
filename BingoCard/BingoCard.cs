3cd5566ddcc264de1de13c9fc9ef1b6fb4f7b05d
EasonLin <CoderEasonLin@users.noreply.github.com>
2017-12-08 21:51:19 +0800

    Retry.

--- BingoCard/BingoCard.cs
+++ BingoCard/BingoCard.cs
@@ -8,32 +8,8 @@ namespace BingoCard
     {
         public static string[] GetCard()
         {
-            var BNumbers = NumberGenerator.Generate(min: 1, max: 15, count: 5);
-            var INumbers = NumberGenerator.Generate(min: 16, max: 30, count: 5);
-            var NNumbers = NumberGenerator.Generate(min: 31, max: 45, count: 4);
-            var GNumbers = NumberGenerator.Generate(min: 46, max: 60, count: 5);
-            var ONumbers = NumberGenerator.Generate(min: 61, max: 75, count: 5);
-            return BNumbers.Select(x => "B" + x.ToString()).Concat(
-                   INumbers.Select(x => "I" + x.ToString()).Concat(
-                   NNumbers.Select(x => "N" + x.ToString()).Concat(
-                   GNumbers.Select(x => "G" + x.ToString()).Concat(
-                   ONumbers.Select(x => "O" + x.ToString()))))).ToArray();
+            return new string[24];
         }
     }
 
-    public class NumberGenerator
-    {
-        public static List<int> Generate(int min, int max, int count)
-        {
-            var result = new List<int>();
-            var random = new Random();
-            while(result.Count < count)
-            {
-                var number = random.Next(min, max);
-                if (!result.Contains(number))
-                    result.Add(number);
-            }
-            return result;
-        }
-    }
 }
--- BingoCard/BingoCardTests.cs
+++ BingoCard/BingoCardTests.cs
@@ -14,66 +14,66 @@ namespace BingoCard
             Assert.AreEqual(24, BingoCard.GetCard().Length);
         }
 
-        [Test]
-        public void EachNumberOnCardIsUnique()
-        {
-            var card = BingoCard.GetCard();
-            Assert.AreEqual(card.Length, card.ToList().Distinct().Count());
-        }
+        //[Test]
+        //public void EachNumberOnCardIsUnique()
+        //{
+        //    var card = BingoCard.GetCard();
+        //    Assert.AreEqual(card.Length, card.ToList().Distinct().Count());
+        //}
 
-        [TestCase("B", 5)]
-        [TestCase("I", 5)]
-        [TestCase("N", 4)]
-        [TestCase("G", 5)]
-        [TestCase("O", 5)]
-        public void ColumnContainsCorrectNumberOfItems(string column, int count)
-        {
-            var numbers = BingoCard.GetCard().Where(x => x.StartsWith(column)).ToList();
-            Assert.AreEqual(count, numbers.Count);
-        }
+        //[TestCase("B", 5)]
+        //[TestCase("I", 5)]
+        //[TestCase("N", 4)]
+        //[TestCase("G", 5)]
+        //[TestCase("O", 5)]
+        //public void ColumnContainsCorrectNumberOfItems(string column, int count)
+        //{
+        //    var numbers = BingoCard.GetCard().Where(x => x.StartsWith(column)).ToList();
+        //    Assert.AreEqual(count, numbers.Count);
+        //}
 
-        [Test]
-        public void NumbersAreOrderedByColumn()
-        {
-            var columns = string.Join("", BingoCard.GetCard().ToList()
-                .Select(x => x.Substring(0, 1)).ToArray());
+        //[Test]
+        //public void NumbersAreOrderedByColumn()
+        //{
+        //    var columns = string.Join("", BingoCard.GetCard().ToList()
+        //        .Select(x => x.Substring(0, 1)).ToArray());
 
-            Assert.IsTrue(Regex.IsMatch(columns, "^[B]*[I]*[N]*[G]*[O]*$"));
-        }
+        //    Assert.IsTrue(Regex.IsMatch(columns, "^[B]*[I]*[N]*[G]*[O]*$"));
+        //}
 
-        [TestCase("B", 1, 15)]
-        [TestCase("I", 16, 30)]
-        [TestCase("N", 31, 45)]
-        [TestCase("G", 46, 60)]
-        [TestCase("O", 61, 75)]
-        public void NumbersWithinColumnAreAllInTheCorrectRange(string column, int start, int end)
-        {
-            var numbers = BingoCard.GetCard().Where(x => x.StartsWith(column)).ToList();
+        //[TestCase("B", 1, 15)]
+        //[TestCase("I", 16, 30)]
+        //[TestCase("N", 31, 45)]
+        //[TestCase("G", 46, 60)]
+        //[TestCase("O", 61, 75)]
+        //public void NumbersWithinColumnAreAllInTheCorrectRange(string column, int start, int end)
+        //{
+        //    var numbers = BingoCard.GetCard().Where(x => x.StartsWith(column)).ToList();
 
-            foreach (var number in numbers)
-            {
-                var n = Convert.ToInt32(number.Substring(1));
-                Assert.GreaterOrEqual(n, start, "Column {0} should be in the range between {1} and {2}, found: {3}", column, start, end, number);
-                Assert.LessOrEqual(n, end, "Column {0} should be in the range between {1} and {2}, found: {3}", column, start, end, number);
-            }
-        }
+        //    foreach (var number in numbers)
+        //    {
+        //        var n = Convert.ToInt32(number.Substring(1));
+        //        Assert.GreaterOrEqual(n, start, "Column {0} should be in the range between {1} and {2}, found: {3}", column, start, end, number);
+        //        Assert.LessOrEqual(n, end, "Column {0} should be in the range between {1} and {2}, found: {3}", column, start, end, number);
+        //    }
+        //}
 
-        [Test]
-        public void NumbersWithinColumnAreInRandomOrder()
-        {
-            var card = BingoCard.GetCard().Select(x => Convert.ToInt32(x.Substring(1))).ToArray();
+        //[Test]
+        //public void NumbersWithinColumnAreInRandomOrder()
+        //{
+        //    var card = BingoCard.GetCard().Select(x => Convert.ToInt32(x.Substring(1))).ToArray();
 
-            var isRandom = false;
-            for (var i = 1; i < card.Length; i++)
-            {
-                if (card[i - 1] > card[i])
-                {
-                    isRandom = true;
-                    break;
-                }
-            }
+        //    var isRandom = false;
+        //    for (var i = 1; i < card.Length; i++)
+        //    {
+        //        if (card[i - 1] > card[i])
+        //        {
+        //            isRandom = true;
+        //            break;
+        //        }
+        //    }
 
-            Assert.IsTrue(isRandom, "Unlikely result, is the list ordered?");
-        }
+        //    Assert.IsTrue(isRandom, "Unlikely result, is the list ordered?");
+        //}
     }
 }

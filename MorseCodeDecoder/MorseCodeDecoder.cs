272c1b055e6ceb11e1aac6ad3ef84b95fb341fdd
EasonLin <CoderEasonLin@users.noreply.github.com>
2017-12-09 23:28:56 +0800

    Replace dictionary mapping to MorseCode.Get.

--- MorseCodeDecoder/MorseCodeDecoder.cs
+++ MorseCodeDecoder/MorseCodeDecoder.cs
@@ -1,62 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 
 public class MorseCodeDecoder
 {
-    private static Dictionary<string, string> _morseCodeMapping;
-
     public static string Decode(string input)
     {
-        _morseCodeMapping = new Dictionary<string, string>
-        {
-            {".- ", "A"},
-            {"-... ", "B"},
-            {"-.-. ", "C"},
-            {"-.. ", "D"},
-            {". ", "E"},
-            {"..-. ", "F"},
-            {"--. ", "G"},
-            {".... ", "H"},
-            {".. ", "I"},
-            {".--- ", "J"},
-            {"-.- ", "K"},
-            {".-.. ", "L"},
-            {"-- ", "M"},
-            {"-. ", "N"},
-            {"--- ", "O"},
-            {".--. ", "P"},
-            {"--.- ", "Q"},
-            {".-. ", "R"},
-            {"... ", "S"},
-            {"- ", "T"},
-            {"..- ", "U"},
-            {"...- ", "V"},
-            {".-- ", "W"},
-            {"-..- ", "X"},
-            {"-.-- ", "Y"},
-            {"--.. ", "Z"},
-            {".---- ", "1"},
-            {"..--- ", "2"},
-            {"...-- ", "3"},
-            {"....- ", "4"},
-            {"..... ", "5"},
-            {"-.... ", "6"},
-            {"--... ", "7"},
-            {"---.. ", "8"},
-            {"----. ", "9"},
-            {"----- ", "0"},
-            {"...---... ", "SOS"},
-            {"-.-.-- ", "!"},
-            {".-.-.- ", "."},
-            {"  ", " "},
-        };
-        
-        input = $"{input.Trim()} ";
-        foreach (var mapping in _morseCodeMapping.OrderByDescending(pair => pair.Key))
-        {
-            input = input.Replace(mapping.Key, mapping.Value);
-        }
-        return input;
+        var words = input.Trim().Split(new[] {"   "}, StringSplitOptions.None);
+        return string.Join(" ", words.Select(Translate));
+    }
+
+    private static string Translate(string word)
+    {
+        return string.Join("", word.Split(' ').Select(MorseCode.Get));
     }
 }
